33d53f177b778a4072b38eb75ce2e877d49e465e
Charly <cramirezdiaz.dev@gmail.com>
2026-03-07 11:03:51 -0300

    galeria

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tiempoconstrucciones.com',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
-  }
+  },
+  integrations: [sitemap()]
 });
